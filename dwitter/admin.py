4bae60cc9cd1d712b0055f0924be367bdbcaf793
unknown <meander.training@gmail.com>
2023-11-18 00:14:16 -0800

    social tweet app

---
+++ dwitter/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.models import User,Group
+from .models import Dweet, Profile
+# class UserAdmin(admin.ModelAdmin):
+#     model = User
+#     # Only display the "username" field
+#     fields = ["username"]
+
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Dweet)
+
+
+
+#admin.site.register(Profile)
